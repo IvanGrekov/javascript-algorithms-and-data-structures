1c3cf748fc24cc30082383214024ea47921363cf
Ivan Grekov <grekovv76@gmail.com>
2026-09-20 20:01:28 +0300

    break it down step

---
+++ section4_problem_solving_approach/step3_break_it_down.js
@@ -0,0 +1,13 @@
+// 1. Explicitly write out the steps you need to take. Just the basic components of the solution.
+// 2. This forces you to think about the code you'll write before you write it. It also helps you catch any potential issues or difficulties.
+
+// ------------
+
+// Example: Write a function that takes in a string and returns counts of each character in the string.
+// function charCount(str) {
+// - make an object to return at the end;
+// - loop over the string;
+// - sanitize the character by making it lowercase and skipping not suitable characters;
+// - check if key exists before incrementing;
+// - return object at end;
+// }
