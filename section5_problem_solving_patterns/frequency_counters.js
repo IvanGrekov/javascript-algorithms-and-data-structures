76940b7d42ebff02e1a1ea8887cf07601e2824da
Ivan Grekov <grekovv76@gmail.com>
2026-09-23 18:57:07 +0300

    refactor frequency counter file

--- section5_problem_solving_patterns/frequency_counters.js
+++ section5_problem_solving_patterns/frequency_counters.js
@@ -6,9 +6,6 @@
 // EXAMPLE 1: Write a function called same, which accepts two arrays.
 // The function should return true if every value in the array has its corresponding value squared in the second array.
 // The frequency of values must be the same.
-// same([1,2,3], [4,1,9]) // true
-// same([1,2,3], [1,9]) // false
-// same([1,2,1], [4,4,1]) // false (must be same frequency)
 
 // O(n)
 function same(arr1, arr2) {
@@ -39,20 +36,13 @@ function getFrequenciesFromArray(arr) {
     return acc;
 }
 
-// console.log(same([1, 2, 3], [4, 1, 9]))
-// console.log(same([1, 2, 3], [1, 9]))
-// console.log(same([1, 2, 1], [4, 4, 1]))
-// console.log(same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]))
+// console.log(same([1, 2, 3], [4, 1, 9])) // true
+// console.log(same([1, 2, 3], [1, 9])) // false
+// console.log(same([1, 2, 1], [4, 4, 1])) // false (must be same frequency)
+// console.log(same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11])) // false
 
 // EXAMPLE 2: Anagrams. Given two strings, write a function to determine if the second string is an anagram of the first.
 // An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
-// validAnagram('', '') // true
-// validAnagram('aaz', 'zza') // false
-// validAnagram('anagram', 'nagaram') // true
-// validAnagram('rat', 'car') // false
-// validAnagram('awesome', 'awesom') // false
-// validAnagram('qwerty', 'qeywrt') // true
-// validAnagram('texttwisttime', 'timetwisttext') // true
 
 // O(n)
 function validAnagram(str1, str2) {
@@ -76,7 +66,10 @@ function validAnagram(str1, str2) {
     return true;
 }
 
-console.log(validAnagram('', ''));
-console.log(validAnagram('aaz', 'zza'));
-console.log(validAnagram('anagram', 'nagaram'));
-console.log(validAnagram('rat', 'car'));
+console.log(validAnagram('', '')); // true
+console.log(validAnagram('aaz', 'zza')); // false
+console.log(validAnagram('anagram', 'nagaram')); // true
+console.log(validAnagram('rat', 'car')); // false
+console.log(validAnagram('awesome', 'awesom')) // false
+console.log(validAnagram('qwerty', 'qeywrt')) // true
+console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
