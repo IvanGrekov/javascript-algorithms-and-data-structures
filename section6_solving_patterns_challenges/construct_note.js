8ce57d1c8d9559c316d86ff66858275bf83ebe29
Ivan Grekov <grekovv76@gmail.com>
2026-09-26 08:50:29 +0300

    construct node challenge

---
+++ section6_solving_patterns_challenges/construct_note.js
@@ -0,0 +1,35 @@
+// Write a function called constructNote, which accepts two strings, a message and some letters.
+// The function should return true if the message can be built with the letters that you are given,
+// or it should return false.
+
+// Assume that there are only lowercase letters and no space or special characters in both the message and the letters.
+
+// Bonus Constraints:
+// If M is the length of message and N is the length of letters:
+// Time Complexity: O(M + N)
+// Space Complexity: O(N)
+
+function constructNote(message, letters) {
+    if (message.length > letters.length) {
+        return false;
+    }
+
+    const lettersDictionary = {};
+    for (const char of letters) {
+        lettersDictionary[char] = ++lettersDictionary[char] || 1;
+    }
+
+    for (const char of message) {
+        if (!lettersDictionary[char]) {
+            return false;
+        }
+
+        --lettersDictionary[char];
+    }
+
+    return true;
+}
+
+console.log(constructNote('aa', 'abc')) // false
+console.log(constructNote('abc', 'dcba')) // true
+console.log(constructNote('aabbcc', 'bcabcaddff')) // true
